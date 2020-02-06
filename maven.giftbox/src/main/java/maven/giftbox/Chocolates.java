b5cb0c47f6d5a61fb87aab5588897ab17a01f173
rushali <rushaliasthana@gmail.com>
2020-02-06 14:13:52 +0530

    commit

--- maven.giftbox/src/main/java/maven/giftbox/Chocolates.java
+++ maven.giftbox/src/main/java/maven/giftbox/Chocolates.java
@@ -15,14 +15,7 @@ public class Chocolates {
 		this.cost=cost;
 		this.weight=weight;
 	}
-//	public Chocolates(String type,int quantity,String chocname,double cost,double weight)
-//	{
-//		this.type=type;
-//		this.quantity=quantity;
-//		this.chocname=chocname;
-//		this.cost=cost;
-//		this.weight=weight;
-//	}
+
 	public Chocolates(String chocname,double cost,double weight)
 	{
 		
@@ -31,34 +24,5 @@ public class Chocolates {
 		this.weight=weight;
 	}
 	
-//	public void setType(String type){
-//	this.type=type;
-//	}
-//	public void setQuan(int quantity){
-//		this.quantity=quantity;
-//		}
-//	public void setChocname(String chocname){
-//		this.chocname=chocname;
-//		}
-//	public void setCost(double cost){
-//		this.cost=cost;
-//		}
-//	public void setWeight(double weight){
-//		this.weight=weight;
-//		}
-//	public String getType(){
-//	return type;
-//	}
-//	public int getQuan(){
-//	return quantity;
-//	}
-//	public double getCost(){
-//	return cost;
-//	}
-//	public String getChocname(){
-//	return chocname;
-//	}
-//	public double getWight(){
-//	return weight;
-//	}
+
 }
